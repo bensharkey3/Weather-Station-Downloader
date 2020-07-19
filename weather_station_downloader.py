10e8a580fdee90b6cb369fbb186a2ac100e50319
Ben Sharkey <bensharkey3@gmail.com>
2020-07-19 16:44:19 +1000

    updated to add to aws lambda

--- weather_station_downloader.py
+++ weather_station_downloader.py
@@ -1,8 +1,8 @@
-import urllib.request
-import datetime
-import os
-import logging
+import json
+import boto3
 import smtplib
+import datetime
+import requests
 
 
 class ImageNameUrl:
@@ -10,9 +10,9 @@ class ImageNameUrl:
         '''creates an object for each image containing its url and giving it a name'''
         self.name = name
         self.url = url
-
         
-def download_image(url, directory, name):
+        
+def download_image(url, name, bucket_name):
     '''Retrieves an image from the provided url, and saves to the directory provided.
     Args:
         url (str): the image address
@@ -21,8 +21,14 @@ def download_image(url, directory, name):
     Returns:
         none
     '''
-    path = directory + '/' + datetime.datetime.today().strftime('%Y-%m-%d') + '-' + name +'.jpg'
-    urllib.request.urlretrieve(url, path)
+#     path = directory + '/' + datetime.datetime.today().strftime('%Y-%m-%d') + '-' + name +'.jpg'
+#     urllib.request.urlretrieve(url, path)
+
+    object_key = datetime.datetime.today().strftime('%Y-%m-%d') + '-' + name +'.png'
+    s3_object = boto3.resource('s3').Object(bucket_name, object_key)
+    
+    with requests.get(url, stream=True) as r:
+        s3_object.put(Body=r.content)
     
 
 def send_basic_gmail(to_email, subject, message_body):
@@ -49,22 +55,13 @@ def send_basic_gmail(to_email, subject, message_body):
     server.quit()
 
 
-def main():  
-    '''Runs the program'''
-    # create and configure logger 
-    logging.basicConfig(filename="logs.log", 
-        format='%(asctime)s - %(levelname)s - %(message)s', 
-        datefmt='%Y-%m-%d %H:%M:%S',
-        filemode='a') 
-
-    logger=logging.getLogger() 
-    logger.setLevel(20) 
-
-    # define variables
+def lambda_handler(event, context):
+    '''runs program
+    '''
+    bucket_name = 'cape-lambert-weather-station-images'
     success_count = 0
     fail_count = 0
-    directory = str(os.getcwd())
-
+    
     meterologystation = ImageNameUrl('meterologystation', r'http://www.pilbarairon.com/DprCLWeather/GraphInfo/CLW_Met.Gif')
     capelambertdolphin34 = ImageNameUrl('capelambertdolphin34', r'http://www.pilbarairon.com/DprCLWeather/GraphInfo/D34_Wave.Gif')
     capelambertbeacon28 = ImageNameUrl('capelambertbeacon28', r'http://www.pilbarairon.com/DprCLWeather/GraphInfo/M28_Met.Gif')
@@ -76,27 +73,20 @@ def main():
                   capelambertbeacon28,
                   beacon14wavesignificant,
                   beacon14wavemax]
-
+    
     # download images
     for i in image_list:
         try:
-            download_image(i.url, directory, i.name)
+            download_image(i.url, i.name, bucket_name)
             success_count+=1
         except Exception as ex:
             fail_count+=1
             print(i.name + ': ' + str(ex))
-            logger.error(i.name + ': ' + str(ex))
     
-    # write completion messages to logs and email
-    if fail_count == 0:
-        message = 'Successful - {} images downloaded'.format(success_count)
-    else:
-        message = 'ERROR - {} images downloaded, {} failed'.format(success_count, fail_count)
-        
-    print(message)
-    logger.info(message)
+    message = 'downloaded {} images using aws lambda - {} errors encountered'.format(success_count, fail_count)
     send_basic_gmail('bensharkey3@gmail.com', message, '')
-
     
-if __name__ == '__main__':
-    main()
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
