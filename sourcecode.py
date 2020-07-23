170a940224b389805547bb1bca86e4da079adad9
Ben Sharkey <bensharkey3@gmail.com>
2020-07-23 10:49:56 +1000

    deleted unnecessary file

--- sourcecode.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-
-code = 'https://raw.githubusercontent.com/bensharkey3/Weather-Station-Downloader/master/weather_station_downloader.py'
-response = urllib.request.urlopen(code)
-data = response.read()
-exec(data)
